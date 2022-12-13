ecd9527fda368bcabca4dccdf131f6599b1af71a
hs <hysokim1@gmail.com>
2022-12-13 15:13:15 +0900

    5

--- src/main/java/com/myshop/shop/controller/SampleController.java
+++ src/main/java/com/myshop/shop/controller/SampleController.java
@@ -1,6 +1,5 @@
 package com.myshop.shop.controller;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
