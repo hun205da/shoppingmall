d819d9c459ea9b36b7797206981a10ce6acb46b3
hs <hysokim1@gmail.com>
2022-12-13 15:13:02 +0900

    5

---
+++ src/main/java/com/myshop/shop/controller/SampleController.java
@@ -0,0 +1,17 @@
+package com.myshop.shop.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@Log4j2
+public class SampleController {
+    @GetMapping("/basic")
+    public void basic(){
+
+    }
+}
