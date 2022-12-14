1d4ceeffd3f2e1e03513f0e74417bab516f43e66
user <caker456@naver.com>
2022-12-14 10:28:36 +0900

    testfile 2022/12/14 9:56

--- src/main/java/com/myshop/shop/entity/User.java
+++ src/main/java/com/myshop/shop/entity/User.java
@@ -4,15 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "Product")
-@ToString
-@Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long product_number;
+
 }
