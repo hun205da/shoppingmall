6f05331eac8d5be1b5e8f6e45cf0ac16b700724b
jhhy1557 <jjhyuk96@gmail.com>
2022-12-15 17:18:42 +0900

    게시판 프로젝트 변수 DTO 이름 바꿔서 적용

---
+++ src/main/java/com/myshop/shop/repository/CsRepository.java
@@ -0,0 +1,8 @@
+package com.myshop.shop.repository;
+
+import com.myshop.shop.entity.Cs;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface CsRepository extends JpaRepository<Cs, Long>, QuerydslPredicateExecutor<Cs> {
+}
