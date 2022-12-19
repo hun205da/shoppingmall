4df6f9ebaed6ce9670194716ec63419de4553a89
HyeonSoo1 <92965006+HyeonSoo1@users.noreply.github.com>
2022-12-19 15:56:00 +0900

    Delete UserRepository.java

--- src/main/java/com/myshop/shop/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myshop.shop.repository;
-
-import com.myshop.shop.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
