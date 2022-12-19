db5a717f09f0b25da6aa3eee0d546ed84cdd9f05
hs <hysokim1@gmail.com>
2022-12-19 15:40:51 +0900

    a

--- src/main/java/com/myshop/shop/config/SecurityConfig.java
+++ src/main/java/com/myshop/shop/config/SecurityConfig.java
@@ -1,4 +1,37 @@
 package com.myshop.shop.config;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@Log4j2
 public class SecurityConfig {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((auth) -> {
+            auth.antMatchers().permitAll();
+            auth.antMatchers("/list").hasRole("ADMIN");
+            auth.antMatchers("/basic").hasRole("USER");
+        });
+
+        http.formLogin();
+        http.csrf().disable();
+        http.logout();
+
+        return http.build();
+    }
+
 }
