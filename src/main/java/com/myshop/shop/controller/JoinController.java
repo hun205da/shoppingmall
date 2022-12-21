ccd285a51853c5a6669e408570c33095de25f935
user <hun205da@naver.com>
2022-12-21 19:19:16 +0900

    갓뎀 메인유아이

---
+++ src/main/java/com/myshop/shop/controller/JoinController.java
@@ -0,0 +1,29 @@
+package com.myshop.shop.controller;
+
+import com.myshop.shop.dto.UserDTO;
+import com.myshop.shop.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/layout")
+@RequiredArgsConstructor
+public class JoinController {
+    private final UserService service;
+    @GetMapping("/join")
+    public void join(){
+    }
+    @PostMapping("/join")
+    public String joinPost(UserDTO dto, RedirectAttributes redirectAttributes) {
+        String userId = service.userdata(dto);
+
+        redirectAttributes.addFlashAttribute("userid",userId);
+
+        return "redirect:/layout/basic";
+    }
+}
