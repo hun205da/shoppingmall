dbf4e81d00a3cf52c9c5851f9143fd91e126c826
user <caker456@naver.com>
2022-12-27 15:28:42 +0900

    헤더푸터적용

--- src/main/java/com/myshop/shop/controller/MasterController.java
+++ src/main/java/com/myshop/shop/controller/MasterController.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MasterController {
     private final UserService service;
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/layout/masterlist/masterlistGest";
+    }
     @GetMapping("/masterlistHome")
     public void masterlistHome(){
 
@@ -29,6 +34,7 @@ public class MasterController {
     public void masterlistGest(PageRequestDTO pagerequestDTO, Model model){
         model.addAttribute("userdto",service.getListDto(pagerequestDTO));
     }
+
     @GetMapping("/masterlistList")
     public void masterlistList(){
 
