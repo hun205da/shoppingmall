06f1f26b070c1eb3e71fa185d2def20731e8df3a
User_Name <jjhyuk96@gmail.com>
2023-01-01 05:39:45 +0900

    기능끝

--- src/main/java/com/myshop/shop/entity/Member.java
+++ src/main/java/com/myshop/shop/entity/Member.java
@@ -39,7 +39,7 @@ public class Member extends BaseEntity {
         member.setAddress(memberFormDto.getAddress());
         String password = passwordEncoder.encode(memberFormDto.getPassword());
         member.setPassword(password);
-        member.setRole(Role.ADMIN);
+        member.setRole(Role.USER);
         return member;
 
     }
