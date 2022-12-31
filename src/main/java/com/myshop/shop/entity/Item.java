84e92d7097c37e686f40299f6278d5174076fb56
User_Name <jjhyuk96@gmail.com>
2022-12-31 18:33:03 +0900

    300

--- src/main/java/com/myshop/shop/entity/Item.java
+++ src/main/java/com/myshop/shop/entity/Item.java
@@ -2,6 +2,7 @@ package com.myshop.shop.entity;
 
 import com.myshop.shop.constant.ItemSellStatus;
 import com.myshop.shop.dto.ItemFormDto;
+import com.myshop.shop.exception.OutOfStockException;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -41,6 +42,14 @@ public class Item extends BaseEntity{
         this.stockNumber = itemFormDto.getStockNumber();
         this.itemDetail = itemFormDto.getItemDetail();
         this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
+
+    public void removeStock(int stockNumber){
+        int restStock = this.stockNumber - stockNumber;
+        if(restStock<0){
+            throw new OutOfStockException("상품의 재고가 부족 합니다. (현재 재고 수량: " + this.stockNumber + ")");
+        }
+        this.stockNumber = restStock;
     }
 
 }
---
+++ src/main/java/com/myshop/shop/exception/OutOfStockException.java
@@ -0,0 +1,9 @@
+package com.myshop.shop.exception;
+
+public class OutOfStockException extends RuntimeException{
+
+    public OutOfStockException(String message) {
+        super(message);
+    }
+
+}
