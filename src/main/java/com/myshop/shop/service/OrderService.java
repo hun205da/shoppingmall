93e6c96d8f9982d2a8693d9a5b1482a8af73d0f0
User_Name <jjhyuk96@gmail.com>
2022-12-31 19:43:22 +0900

    306

--- src/main/java/com/myshop/shop/service/OrderService.java
+++ src/main/java/com/myshop/shop/service/OrderService.java
@@ -35,8 +35,6 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    private final ItemImgRepository itemImgRepository;
-
     public Long order(OrderDto orderDto, String email){
 
         Item item = itemRepository.findById(orderDto.getItemId())
