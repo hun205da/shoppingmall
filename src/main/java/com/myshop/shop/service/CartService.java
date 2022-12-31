4e745b06fbaf37f2f174810b114fa2da97ce8a42
User_Name <jjhyuk96@gmail.com>
2022-12-31 22:54:28 +0900

    354

--- src/main/java/com/myshop/shop/service/CartService.java
+++ src/main/java/com/myshop/shop/service/CartService.java
@@ -93,12 +93,12 @@ public class CartService {
 
         cartItem.updateCount(count);
     }
-//
-//    public void deleteCartItem(Long cartItemId) {
-//        CartItem cartItem = cartItemRepository.findById(cartItemId)
-//                .orElseThrow(EntityNotFoundException::new);
-//        cartItemRepository.delete(cartItem);
-//    }
+
+    public void deleteCartItem(Long cartItemId) {
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(EntityNotFoundException::new);
+        cartItemRepository.delete(cartItem);
+    }
 //
 //    public Long orderCartItem(List<CartOrderDto> cartOrderDtoList, String email){
 //        List<OrderDto> orderDtoList = new ArrayList<>();
