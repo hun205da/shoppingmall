d0cf3229367fc42a0adc63fd9648b399d8667569
User_Name <jjhyuk96@gmail.com>
2022-12-31 17:56:28 +0900

    270

---
+++ src/main/java/com/myshop/shop/dto/ItemSearchDto.java
@@ -0,0 +1,18 @@
+package com.myshop.shop.dto;
+
+import com.myshop.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ItemSearchDto {
+
+    private String searchDateType;
+
+    private ItemSellStatus searchSellStatus;
+
+    private String searchBy;
+
+    private String searchQuery = "";
+
+}
---
+++ src/main/java/com/myshop/shop/repository/ItemRepositoryCustom.java
@@ -0,0 +1,16 @@
+package com.myshop.shop.repository;
+
+
+import com.myshop.shop.dto.ItemSearchDto;
+import com.myshop.shop.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+public interface ItemRepositoryCustom {
+
+    Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+//    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+}
