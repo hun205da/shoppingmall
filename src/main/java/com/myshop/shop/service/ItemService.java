4bfab24b10185044b548bad6e608b5aca325cdae
User_Name <jjhyuk96@gmail.com>
2022-12-31 17:46:50 +0900

    262

--- src/main/java/com/myshop/shop/service/ItemService.java
+++ src/main/java/com/myshop/shop/service/ItemService.java
@@ -63,22 +63,22 @@ public class ItemService {
         itemFormDto.setItemImgDtoList(itemImgDtoList);
         return itemFormDto;
     }
-//
-//    public Long updateItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
-//        //상품 수정
-//        Item item = itemRepository.findById(itemFormDto.getId())
-//                .orElseThrow(EntityNotFoundException::new);
-//        item.updateItem(itemFormDto);
-//        List<Long> itemImgIds = itemFormDto.getItemImgIds();
-//
-//        //이미지 등록
-//        for(int i=0;i<itemImgFileList.size();i++){
-//            itemImgService.updateItemImg(itemImgIds.get(i),
-//                    itemImgFileList.get(i));
-//        }
-//
-//        return item.getId();
-//    }
+
+    public Long updateItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
+        //상품 수정
+        Item item = itemRepository.findById(itemFormDto.getId())
+                .orElseThrow(EntityNotFoundException::new);
+        item.updateItem(itemFormDto);
+        List<Long> itemImgIds = itemFormDto.getItemImgIds();
+
+        //이미지 등록
+        for(int i=0;i<itemImgFileList.size();i++){
+            itemImgService.updateItemImg(itemImgIds.get(i),
+                    itemImgFileList.get(i));
+        }
+
+        return item.getId();
+    }
 //
 //    @Transactional(readOnly = true)
 //    public Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable){
