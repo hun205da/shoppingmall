fdff749e1e2501e7a8cdff37143dd38104fc2c05
user <caker456@naver.com>
2023-01-04 15:42:52 +0900

    cart ui & masteruser list

--- src/main/java/com/myshop/shop/repository/MemberRepository.java
+++ src/main/java/com/myshop/shop/repository/MemberRepository.java
@@ -2,8 +2,9 @@ package com.myshop.shop.repository;
 
 import com.myshop.shop.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
 
     Member findByEmail(String email);
 }
--- src/main/java/com/myshop/shop/service/UserServiceImpl.java
+++ src/main/java/com/myshop/shop/service/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.myshop.shop.service;
 
 import com.myshop.shop.dto.*;
 import com.myshop.shop.entity.Member;
+import com.myshop.shop.entity.QMember;
 import com.myshop.shop.repository.MemberRepository;
+import com.querydsl.core.BooleanBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
@@ -24,12 +26,39 @@ public class UserServiceImpl implements UserService{
     public PageResultDTO<CakerMemberDTO, Member> getListDto(PageRequestDTO requestDTO) {
 
         Pageable pageable = requestDTO.getPageable(Sort.by("id").descending());
-
-        Page<Member> result = repository.findAll(pageable); //Querydsl 사용
+        BooleanBuilder booleanBuilder = getSearch(requestDTO);
+        Page<Member> result = repository.findAll(booleanBuilder,pageable); //Querydsl 사용
 
         Function<Member,CakerMemberDTO> fn = (entity -> UserEntityToDTO(entity));
 
         return new PageResultDTO<>(result, fn);
     }
+    private BooleanBuilder getSearch(PageRequestDTO requestDTO){
 
+        String type = requestDTO.getType();
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        String keyword = requestDTO.getKeyword();
+        QMember qmember = QMember.member;
+        if(type == null || type.trim().length() == 0){ //검색 조건이 없는 경우
+            return booleanBuilder;
+        }
+
+        //검색 조건을 작성하기
+        BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+        if(type.contains("uemail")){
+            conditionBuilder.or(qmember.email.contains(keyword));
+        }
+        if(type.contains("uname")){
+            conditionBuilder.or(qmember.name.contains(keyword));
+        }
+        if(type.contains("urole")){
+            conditionBuilder.or(qmember.role.stringValue().contains(keyword));
+        }
+
+        //모든 조건 통합
+        booleanBuilder.and(conditionBuilder);
+
+        return booleanBuilder;
+    }
 }
